a93a4ad2160694a8234c42ec55625282a30f1b5a
John Duncan <john@johnduncanphoto.com>
2017-11-17 18:50:32 +0000

    database classes created

--- app/src/main/java/com/example/johnd/androidstudiotodolist/DatabaseHelper.java
+++ app/src/main/java/com/example/johnd/androidstudiotodolist/DatabaseHelper.java
@@ -1,8 +1,28 @@
 package com.example.johnd.androidstudiotodolist;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
 /**
  * Created by JohnD on 17/11/2017.
  */
 
-public class DatabaseHelper {
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "todolist.db";
+    public static final String TABLE_NAME = "list_table";
+    public static final String COL_1 = "ID";
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
