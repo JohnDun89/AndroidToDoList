697386346c8840accac9292724ef0859721a5fe8
John Duncan <john@johnduncanphoto.com>
2017-11-17 17:10:32 +0000

    listItem class created

--- app/src/main/java/com/example/johnd/androidstudiotodolist/List.java
+++ app/src/main/java/com/example/johnd/androidstudiotodolist/List.java
@@ -1,8 +1,16 @@
 package com.example.johnd.androidstudiotodolist;
 
+import java.util.ArrayList;
+
 /**
  * Created by JohnD on 17/11/2017.
  */
 
 public class List {
+
+    private ArrayList<ListItem> ToDoList;
+
+    public List(){
+        ToDoList = new ArrayList<ListItem>();
+    }
 }
---
+++ app/src/main/java/com/example/johnd/androidstudiotodolist/ListItem.java
@@ -0,0 +1,13 @@
+package com.example.johnd.androidstudiotodolist;
+
+/**
+ * Created by JohnD on 17/11/2017.
+ */
+
+public class ListItem {
+
+    private int id;
+    private String name;
+    private String description;
+    private boolean completed;
+}
