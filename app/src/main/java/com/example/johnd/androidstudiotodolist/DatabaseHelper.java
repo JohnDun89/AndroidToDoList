d0d027ba226dea13d2b190178749fcb76286ca19
John Duncan <john@johnduncanphoto.com>
2017-11-19 21:44:19 +0000

    get all items form database created

--- app/src/main/java/com/example/johnd/androidstudiotodolist/DatabaseHelper.java
+++ app/src/main/java/com/example/johnd/androidstudiotodolist/DatabaseHelper.java
@@ -33,7 +33,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
 
-
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, TITLE TEXT, DESCRIPTION TEXT, COMPLETED BOOLEAN, PRIORITY BIGINT) ");
@@ -42,27 +41,27 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-        sqLiteDatabase.execSQL(" DROP TABLE IF EXISTS " + TABLE_NAME );
+        sqLiteDatabase.execSQL(" DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(sqLiteDatabase);
     }
 
 
     // not sure the following is required - if issues remove.
-    public static DatabaseHelper newInstance(Context context){
-        if (mDatabaseInstance == null){
+    public static DatabaseHelper newInstance(Context context) {
+        if (mDatabaseInstance == null) {
             mDatabaseInstance = new DatabaseHelper(context.getApplicationContext());
         }
         return mDatabaseInstance;
     }
 
-    public boolean insertData(String title, String description, String priority ){
+    public boolean insertData(String title, String description, String priority) {
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_TITLE, title);
         contentValues.put(COL_DESCRIPTION, description);
         contentValues.put(COL_COMPLETED, "false");
         contentValues.put(COL_PRIORITY, priority);
-        sqLiteDatabase.insert(TABLE_NAME,null,contentValues);
+        sqLiteDatabase.insert(TABLE_NAME, null, contentValues);
         return true;
     }
 
@@ -72,22 +71,26 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 //        return result;
 //    }
 
-    public ArrayList<ListItem> getAllItems(){
+    public ArrayList<ListItem> getAllItems() {
         SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
         Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM " + TABLE_NAME, null);
         ArrayList<ListItem> list = new ArrayList<ListItem>();
         ListItem listItem;
-        if (cursor.getCount() > 0 ){
-            for (int i = 0; i < cursor.getCount(); i++);
-            cursor.moveToNext();
-            listItem = new ListItem();
-            listItem.setID(cursor.getString(0));
-            listItem.setname(cursor.getString(1));
-            listItem.setDescription(cursor.getString(2));
-            listItem.setCompleted(cursor.getString(3));
-            listItem.setPriority(cursor.getString());
+        if (cursor.getCount() > 0) {
+            for (int i = 0; i < cursor.getCount(); i++) {
+                cursor.moveToNext();
+                listItem = new ListItem();
+                listItem.setID(cursor.getString(0));
+                listItem.setname(cursor.getString(1));
+                listItem.setDescription(cursor.getString(2));
+                listItem.setCompleted(cursor.getString(3));
+                listItem.setPriority(cursor.getString(4));
+                list.add(listItem);
+            }
         }
+        cursor.close();
+        sqLiteDatabase.close();
+        return list;
+
     }
-
-
 }
